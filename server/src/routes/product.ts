313127b583cd9b153eaa8396952e6a54da1da34f
hashimkhanzada <hashimkhanzada@gmail.com>
2021-03-27 02:11:39 +1300

    Search functionality

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -7,20 +7,29 @@ import {
   getProductByMainCategory,
   seedProducts,
   getSubCategoryData,
+  getProductsBySearch,
+  getMainCategoryData,
 } from "../controllers/products";
 
 const router = express.Router();
 
 router.get("/", expressAsyncHandler(getProducts));
+
 router.get("/seed", expressAsyncHandler(seedProducts));
+
+router.get("/search", expressAsyncHandler(getProductsBySearch));
+router.get("/search/a", expressAsyncHandler(getMainCategoryData));
+
 router.get(
   "/browse/:mainCategory/data",
   expressAsyncHandler(getProductByMainCategory)
 );
+
 router.get(
   "/browse/:mainCategory/subcategory",
   expressAsyncHandler(getSubCategoryData)
 );
+
 router.get("/browse/:id", expressAsyncHandler(getProductById));
 
 export default router;
