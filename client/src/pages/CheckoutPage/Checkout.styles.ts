b0395b5ddf41e94957d7c0ee5bc6706fee45b7a9
hashimkhanzada <hashimkhanzada@gmail.com>
2021-03-27 23:54:36 +1300

    Checkout page design

--- client/src/pages/CheckoutPage/Checkout.styles.ts
+++ client/src/pages/CheckoutPage/Checkout.styles.ts
@@ -4,14 +4,130 @@ export const CheckoutContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  color: #39464e;
 `;
 
 export const CheckoutMain = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
   background-color: white;
   width: 100%;
   max-width: 1200px;
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 48px 180px;
+`;
+
+export const DeliveryRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 12px 0;
+  padding-bottom: 30px;
+  border-bottom: solid 1px #889ca8;
+
+  h3 {
+    font-size: 23px;
+  }
+
+  .num {
+    border: 1px solid #536671;
+    border-radius: 1rem;
+    height: 2rem;
+    margin-right: 0.75rem;
+    text-align: center;
+    width: 2rem;
+  }
+
+  button {
+    width: 100%;
+    margin: 10px;
+    padding: 30px 0;
+    background-color: white;
+    font-size: 25px;
+    border: solid 1px #889ca8;
+    border-radius: 5px;
+    color: #39464e;
+
+    p {
+      font-size: 20px;
+      font-weight: bolder;
+    }
+  }
+
+  .selected {
+    background-color: rgba(0, 120, 55, 0.1);
+    color: #007837;
+    cursor: pointer;
+  }
+
+  .address {
+    padding-left: 10px;
+
+    h4 {
+      font-size: 14px;
+      font-weight: normal;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    strong {
+      margin-right: 15px;
+    }
+
+    p {
+      font-size: 14px;
+      color: #007837;
+
+      &:hover {
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export const TimeslotRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 12px 0;
+  padding-bottom: 30px;
+  border-bottom: solid 1px #889ca8;
+
+  .num {
+    border: 1px solid #536671;
+    border-radius: 1rem;
+    height: 2rem;
+    margin-right: 0.75rem;
+    text-align: center;
+    width: 2rem;
+  }
+
+  button {
+    width: 100%;
+    margin: 10px;
+    padding: 15px 0;
+    background-color: white;
+    font-size: 25px;
+    border: solid 1px #889ca8;
+    border-radius: 5px;
+    color: #39464e;
+    cursor: pointer;
+
+    p {
+      font-size: 20px;
+      font-weight: bolder;
+    }
+  }
+
+  .selected {
+    background-color: rgba(0, 120, 55, 0.1);
+    color: #007837;
+  }
 `;
--- client/src/pages/CheckoutPage/Checkout.tsx
+++ client/src/pages/CheckoutPage/Checkout.tsx
@@ -1,10 +1,81 @@
 import React from "react";
-import { CheckoutContainer, CheckoutMain } from "./Checkout.styles";
+import { FiTruck } from "react-icons/fi";
+import { BsBag } from "react-icons/bs";
+
+import {
+  CheckoutContainer,
+  CheckoutMain,
+  MainContainer,
+  DeliveryRow,
+  TimeslotRow,
+} from "./Checkout.styles";
+import { Button } from "../../styles/globalStyles";
 
 const Checkout = () => {
   return (
     <CheckoutContainer>
-      <CheckoutMain>Check</CheckoutMain>
+      <CheckoutMain>
+        <MainContainer>
+          <DeliveryRow>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <h3 className="num">1</h3>
+              <h3>Delivery or Pick up?</h3>
+            </div>
+            <div style={{ display: "flex", margin: "20px 0" }}>
+              <button className="selected">
+                <FiTruck />
+                <p>Delivery</p>
+              </button>
+              <button>
+                <BsBag />
+                <p>Pickup</p>
+              </button>
+            </div>
+            <div className="address">
+              <h4>Deliver to:</h4>
+              <span>
+                <strong>Durie Hill, Whanganui 4500</strong>{" "}
+                <p>Change address {">"}</p>
+              </span>
+            </div>
+          </DeliveryRow>
+          <TimeslotRow>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "10px",
+              }}
+            >
+              <h3 className="num">2</h3>
+              <h3>Choose a day</h3>
+            </div>
+            <p style={{ marginLeft: "10px" }}>Next available slot:</p>
+            <div style={{ display: "flex", margin: "5px 0" }}>
+              <button className="selected">
+                <p>Monday</p>
+              </button>
+              <button>
+                <p>Tuesday</p>
+              </button>
+              <button>
+                <p>Wednesday</p>
+              </button>
+              <button>
+                <p>Thursday</p>
+              </button>
+              <button>
+                <p>Friday</p>
+              </button>
+            </div>
+          </TimeslotRow>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button propWidth="20%" extraMargin="40px 10px">
+              Continue
+            </Button>
+          </div>
+        </MainContainer>
+      </CheckoutMain>
     </CheckoutContainer>
   );
 };
